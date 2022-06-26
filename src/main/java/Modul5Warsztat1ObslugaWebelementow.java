ceb6ac375a519e1474c8a50b4cd2b0650aa6ef78
Sylwia Wojciak <swojciak@dynak.com>
2022-06-26 22:21:34 +0200

    update chromedriver from 101 to 103

---
+++ src/main/java/Modul5Warsztat1ObslugaWebelementow.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Modul5Warsztat1ObslugaWebelementow {
+
+    public static WebDriver driver;
+
+    @Test
+    public void test(){
+
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+
+        driver = new ChromeDriver();
+
+        driver.get("http://www.selenium-shop.pl/");
+
+        driver.manage().window().maximize();
+
+        System.out.println("Tytuł strony: " + driver.getTitle());
+
+        Assert.assertEquals(driver.getTitle(), "Selenium Shop Automatyzacja Testów");
+        
+    }
+
+
+}
